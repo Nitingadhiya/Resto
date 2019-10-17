19b46e1a3e6e737f7ed1f20373ed04194404dba1
Nitingadhiya <vthink.nitin@gmail.com>
2019-10-17 12:09:23 +0530

    change confing file

--- config/server-config.js
+++ config/server-config.js
@@ -1,31 +1,19 @@
-//const path = "https://staging.roadheroes.com";
-const path = "https://roadheroes.com";
-//const path = "http://192.168.1.61:8000";
+const path = 'https://google.com';
 
 const ServerVar = {
   path: path,
-  contactType: "twork",
-  server: "live", //live //staging
-  appURL:
-    "https://play.google.com/store/apps/details?id=com.twork&rdid=com.twork",
+  contactType: 'work',
+  server: 'live', //live //staging
+  appURL: 'https://google.com',
   sentryURL() {
-    if (ServerVar.server === "live") {
-      return "https://e3061180d0e440c48710ce8631181683@sentry.io/1770035";
-    }
-    return "https://e3061180d0e440c48710ce8631181683@sentry.io/1770035";
+    return '';
   },
   privacyPolicyURL() {
-    if (ServerVar.server === "live") {
-      return "https://twork.group/datenschutzerklaerung";
-    }
-    return "https://twork.group/datenschutzerklaerung";
+    return '';
   },
   uxcamURL() {
-    if (ServerVar.server === "live") {
-      return "hk0ct1fqpv1pg3b";
-    }
-    return "hk0ct1fqpv1pg3b";
-  }
+    return '';
+  },
 };
 
 export default ServerVar;
